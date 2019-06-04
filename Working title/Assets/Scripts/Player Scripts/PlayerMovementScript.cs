2c0bd9b0151ad31c9d2d7a41c4d4f822c1b9e220
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2019-06-04 13:25:51 +0100

    Player Movement Script Started
    I have added some minor stuf to the scene: a plane, a player object(placeholder) and a main camera. I have created a folder for holding placeholder materials and a folder for holding scripts.
    
    Inside the Player Scripts folder I have started a player movement script. This script is basic, just using the Input Axis to move on a horizonatal plane. There is a Acceleration variable, a max speed variable and a jump variable.
    
    I have it set up so that an accelerating force will NOT be applied to the player if they go over the max speed. I also set up a fallback so that if they do manage to go over the max speed, their speed is set to the max speed..

---
+++ Working title/Assets/Scripts/Player Scripts/PlayerMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField] int AccelSpeed, Jumpforce, MaxSpeed;
+
+    Rigidbody p_RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_RB = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (p_RB.velocity.x<MaxSpeed&&p_RB.velocity.z<MaxSpeed)
+        { 
+            if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+            {
+                p_RB.AddForce(this.transform.forward * Input.GetAxis("Vertical") * AccelSpeed);
+            }
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+            {
+                p_RB.AddForce(this.transform.right * Input.GetAxis("Horizontal") * AccelSpeed);
+            }
+        }
+        if(Mathf.Abs(p_RB.velocity.x)>MaxSpeed)
+        {
+            p_RB.velocity = new Vector3(MaxSpeed * Input.GetAxis("Horizontal"), p_RB.velocity.y, p_RB.velocity.z);
+        }
+        if (Mathf.Abs(p_RB.velocity.z) > MaxSpeed)
+        {
+            p_RB.velocity = new Vector3(p_RB.velocity.x, p_RB.velocity.y, MaxSpeed * Input.GetAxis("Vertical"));
+        }
+
+    }
+}

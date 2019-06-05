00f99b2742f38c0a4ac65ca9e5b9633cebded3ea
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2019-06-05 15:43:24 +0100

    Primitive Slope Detection
    So I managed to get the slope detection to work. I followed this tutorial on the subject (https://www.youtube.com/watch?v=98MBwtZU2kk). A raycast is emitted towards the ground and is used to alter a custom vector for the forward direction. The forward and backward movement now uses this custom forward vector to move around. I have not done horizontal movement on the slope yet, I still need to work that out.

--- Working title/Assets/Scripts/Player Scripts/PlayerMovementScript.cs
+++ Working title/Assets/Scripts/Player Scripts/PlayerMovementScript.cs
@@ -6,6 +6,14 @@ public class PlayerMovementScript : MonoBehaviour
 {
     [SerializeField] int AccelSpeed, Jumpforce, MaxSpeed;
 
+
+    [SerializeField] Transform raycastPos;
+    RaycastHit raycastHit;
+    float groundAngle;
+    bool grounded;
+
+    Vector3 CustomForward;
+
     Rigidbody p_RB;
     // Start is called before the first frame update
     void Start()
@@ -16,27 +24,84 @@ public class PlayerMovementScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        GroundRaycast();
+        GroundAngleReader();
+        NewForward();
+        Movement();
+
+        Debug.Log(groundAngle);
+        
+    }
+
+    void Movement()
+    {
+        
         if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
         {
-            p_RB.AddForce(this.transform.forward * Input.GetAxis("Vertical") * AccelSpeed);
+            p_RB.AddForce(CustomForward * Input.GetAxis("Vertical") * AccelSpeed);
         }
         if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
         {
             p_RB.AddForce(this.transform.right * Input.GetAxis("Horizontal") * AccelSpeed);
         }
 
+        
+       
 
         // this is the speed limiter for the player.
-        if(p_RB.velocity.magnitude>MaxSpeed)
+        if (p_RB.velocity.magnitude > MaxSpeed)
         {
             p_RB.velocity = p_RB.velocity.normalized * MaxSpeed;
         }
-        
-       
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             p_RB.AddForce(new Vector3(0, Jumpforce, 0));
         }
+    }
+
+    void NewForward()
+    {
+        if(!grounded)
+        {
+            CustomForward = transform.forward;
+            return;
+        }
+
+        CustomForward = Vector3.Cross(transform.right, raycastHit.normal);
+
+    }
+
+
+    void GroundAngleReader()
+    {
+        if(!grounded)
+        {
+            groundAngle = 0f;
+            return;
+        }
+
+        groundAngle = Vector3.Angle(raycastHit.normal, transform.forward);
+    }
+
+    void GroundRaycast()
+    {
+        if(Physics.Raycast(transform.position, -Vector3.up, out raycastHit))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+       
+        Gizmos.DrawLine(this.transform.position, CustomForward.normalized*5+this.transform.position);
     }
 }
